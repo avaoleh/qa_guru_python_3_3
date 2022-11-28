386d95b7fa03635beee2b0c67b0c975a4b527909
AvaOleh <avakimyanov@gmail.com>
2022-11-28 22:16:44 +0300

    complex refactoring

--- test_python_3_3.py
+++ test_python_3_3.py
@@ -1,11 +1,14 @@
-def test_example():
-    assert 5 > 3
+def test_example_resolve():
+    assert 9 > 5
 
-def test_conflict_master():
-    assert 8 > 1
+def test_conflict_master_resolve():
+    assert 11 > 2
 
-def test_conflict_2_master():
-    assert 8 > 5
+def test_conflict_2_master_resolve():
+    assert 10 > 6
 
-def test_conflict_3_master():
+def test_conflict_3_master_resolve():
     assert 8 > 7
+
+def test_conflict_4_master_resolve():
+    assert 29 > 9
